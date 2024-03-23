3d3cc68757ff9548fb51e57c9de3f85ca067aab5
MUHAMMAD CASDI <KNO2104136@smsfinance.co.id>
2024-03-23 15:54:01 +0700

    fixed: config rollup with tslib

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,9 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-
-const packagejson = require("./package.json");
-
+import packagejson from "./package.json" assert {type: 'json'};
 
 export default [
     {
